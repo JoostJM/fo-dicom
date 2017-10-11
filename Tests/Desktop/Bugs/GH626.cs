10a89c0d0cd2000087f258b7778d31f571f9a6b1
Joost van Griethuysen <j.v.griethuysen@nki.nl>
2017-10-11 17:22:59 +0200

    Test: Add Unit test

---
+++ Tests/Desktop/Bugs/GH626.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) 2012-2017 fo-dicom contributors.
+// Licensed under the Microsoft Public License (MS-PL).
+
+namespace Dicom.Bugs
+{
+    using Xunit;
+
+    [Collection("General")]
+    public class GH626
+    {
+        [Fact]
+        public void BadPrivateSequence_Does_Not_Exit_Prematurely()
+        {
+            var d = DicomFile.Open(@"Test Data\GH626.dcm");
+            var pixelData = d.Dataset.Get<DicomItem>(DicomTag.PixelData, -1, null);  // Last tag is expected to be pixeldata
+            Assert.Null(pixelData);
+        }
+    }
+}
